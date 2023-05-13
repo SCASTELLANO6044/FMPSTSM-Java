0296d99fa78a36266296b449318aa6c1e05d2466
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-05-13 12:39:47 +0100

    :building_construction: body of Tabulation

--- Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/Tabulation.java
+++ Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/Tabulation.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Tabulation {
     public static String execute(int[][] matrix){
+        Integer tabulation = tabulation(matrix, new MatrixIndex(0,0));
+        return String.valueOf(tabulation);
+    }
 
-        return "";
+    private static Integer tabulation(int[][]matrix, MatrixIndex matrixIndex){
+
+        int[][] table = new int[matrix.length][matrix[matrix.length-1].length];
+
+        for (int i = 1; i < table.length; i++){
+        }
+
+        return null;
     }
 }
