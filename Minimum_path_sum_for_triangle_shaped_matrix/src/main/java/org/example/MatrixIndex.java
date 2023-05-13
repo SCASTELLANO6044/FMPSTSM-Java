5e8a28d8dfb58e9e31f4891c613032c1c1ee8f58
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-05-13 12:23:03 +0100

    :zap:

--- Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/MatrixIndex.java
+++ Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/MatrixIndex.java
@@ -3,8 +3,8 @@ package org.example;
 import java.util.Objects;
 
 public class MatrixIndex {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public MatrixIndex(int x, int y) {
         this.x = x;
