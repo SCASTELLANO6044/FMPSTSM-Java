304b22fc33aaae2a6f938a326ab4f6cb5d33b15a
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-05-13 19:47:22 +0100

    :sparkles:  First functional implementation of Tabulation

--- Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/Tabulation.java
+++ Minimum_path_sum_for_triangle_shaped_matrix/src/main/java/org/example/Tabulation.java
@@ -1,18 +1,51 @@
 package org.example;
 
+import java.util.*;
+
 public class Tabulation {
-    public static String execute(int[][] matrix){
-        Integer tabulation = tabulation(matrix, new MatrixIndex(0,0));
-        return String.valueOf(tabulation);
+
+    private Tabulation (){
+        throw new IllegalStateException("Utility class");
     }
 
-    private static Integer tabulation(int[][]matrix, MatrixIndex matrixIndex){
+    public static String execute(int[][] matrix){
+        int [][] tabulation = tabulation(matrix);
+        String resultMessage = "The result value is: "+ tabulation[0][0];
+        resultMessage += " and the path chosen is: " + pathChosen(tabulation, matrix);
+        return resultMessage;
+    }
 
-        int[][] table = new int[matrix.length][matrix[matrix.length-1].length];
+    private static int[][] tabulation(int[][]matrix){
 
-        for (int i = 1; i < table.length; i++){
+        int rowAmount = matrix.length;
+        int [][] table = new int[rowAmount][rowAmount];
+        table[rowAmount-1] = Arrays.copyOf(matrix[rowAmount-1], rowAmount);
+        for (int row = rowAmount - 2; row >= 0; row--){
+            for (int col = 0; col <= row; col++){
+                table[row][col] = matrix[row][col] + Integer.min(table[row+1][col], table[row+1][col+1]);
+            }
+        }
+        return table;
+    }
+
+    private static String pathChosen(int[][] table, int[][] originalMatrix){
+        List<MatrixIndex> matrixIndexList = new ArrayList<>(table.length);
+        for (int i = 0; i < table.length; i++){
+            int lessValueItem = Integer.MAX_VALUE;
+            int y = -1;
+            for(int j = 0; j <= i; j++){
+                if (table[i][j] < lessValueItem){
+                    lessValueItem = table[i][j];
+                    y = j;
+                }
+            }
+            matrixIndexList.add(new MatrixIndex(i, y));
         }
 
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (MatrixIndex matrixIndex: matrixIndexList) {
+            stringBuilder.append("[").append(originalMatrix[matrixIndex.getX()][matrixIndex.getY()]).append("]");
+        }
+        return stringBuilder.toString();
     }
 }
